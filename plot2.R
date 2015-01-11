e356982dcfcbfcc262349ede8e847102c3f7fb27
Mash Zahid <mzahid@alineahealth.com>
2015-01-11 13:38:28 -0500

    upload Plot 2 with Image 2

--- plot2.R
+++ plot2.R
@@ -9,10 +9,9 @@ reqData$Date <- as.Date(reqData$Date, format = "%d/%m/%Y")
 # Combine Date/Time into a single column for ease
 reqData$Datetime <- as.POSIXct( paste(as.Date(reqData$Date), reqData$Time, sep = " ") )
 
-# Plot 1 code
-hist(reqData$Global_active_power, main = "Global Active Power",
-     xlab = "Global Active Power (kilowatts)", ylab = "Frequency", col = "Red")
+# Plot 2 code
+plot(reqData$Global_active_power ~ reqData$Datetime, type = "l", ylab = "Global Active Power (kilowatts)", xlab = "")
 
-# Save Image 1 with required dimensions    
-dev.copy(png, file = "plot1.png", height = 480, width = 480)
+# Save Image 2 with required dimensions  
+dev.copy(png, file = "plot2.png", height = 480, width = 480)
 dev.off()
